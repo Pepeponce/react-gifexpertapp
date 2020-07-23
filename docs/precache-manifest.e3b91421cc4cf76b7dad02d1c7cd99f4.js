d5d4a1a8364dcf02bc74f62e4e4807e0ab8a6b09
José Ponce <jponce2492@gmail.com>
2020-07-23 01:43:06 -0400

    docs agregado

---
+++ docs/precache-manifest.e3b91421cc4cf76b7dad02d1c7cd99f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618774304481ffce70bdff0bee135803",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dcd694ec5fefeb3c419",
+    "url": "/static/css/main.c157cc14.chunk.css"
+  },
+  {
+    "revision": "eb00bc0e5d1d87ed4120",
+    "url": "/static/js/2.34972069.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.34972069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dcd694ec5fefeb3c419",
+    "url": "/static/js/main.7dca4e5b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
